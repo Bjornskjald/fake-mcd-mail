8413cda5f05d5f93a8336a62975e9a523cbc31f8
Bjornskjald <github@bjorn.ml>
2019-03-11 15:19:32 +0100

    added changelog, separated common css/js to respective files

---
+++ fullscreen.js
@@ -0,0 +1,14 @@
+document.querySelector('#back').addEventListener('click', e => {
+  // https://developers.google.com/web/fundamentals/native-hardware/fullscreen/
+  const doc = window.document
+  const docEl = doc.documentElement
+
+  const requestFullScreen = docEl.requestFullscreen || docEl.mozRequestFullScreen || docEl.webkitRequestFullScreen || docEl.msRequestFullscreen
+  const cancelFullScreen = doc.exitFullscreen || doc.mozCancelFullScreen || doc.webkitExitFullscreen || doc.msExitFullscreen
+
+  if (!doc.fullscreenElement && !doc.mozFullScreenElement && !doc.webkitFullscreenElement && !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl)
+  } else {
+    cancelFullScreen.call(doc)
+  }
+})
